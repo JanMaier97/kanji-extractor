3584c972c44a487ef99565f40aba025b9e93d78c
Jan Maier <janmaier1997@outlook.de>
2018-08-07 23:55:22 +0200

    Added __init__.py with menu setup and added user_files folder.

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from aqt import mw
+from aqt.qt import QMenu, QAction
+
+
+def create_menu():
+
+    menu = QMenu('Kanji Exctractor', mw)
+    mw.form.menuTools.addAction(menu.menuAction())
+    # mw.form.menuLookup = menu
+
+    create_action = QAction('Create Kanji Notes', mw)
+    create_action.triggered.connect(create_kanji_notes)
+    menu.addAction(create_action)
+
+    settings_action = QAction('Settings', mw)
+    menu.addAction(settings_action)
+    create_action.triggered_conntect(open_settings)
+
+
+def create_kanji_notes():
+    pass
+
+
+def open_settings():
+    pass
+
+
+create_menu()
