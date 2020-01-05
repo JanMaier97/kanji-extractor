4ecc2f6833635ae119052fd1e3a91dfb580989c7
Jan Maier <janmaier1997@outlook.de>
2020-01-05 01:33:48 +0100

    Fixed combobox behaviour for selecting kanji note fields.

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,6 @@ from aqt.utils import showInfo
 from .forms.settings import Ui_Settings 
 
 
-
 class SettingsDialog(QDialog):
     def __init__(self):
         super(SettingsDialog, self).__init__()
@@ -50,6 +49,8 @@ class SettingsDialog(QDialog):
         self.ui.kanji_model.activated.connect(self._on_kanji_model_changed)
         for cb in self._kanji_field_cbs:
             cb.activated.connect(self.on_kanji_cb_activated)
+            if not cb is self.ui.cb_kanji:
+                cb.insertItem(0, '----')
 
         self.ui.kanji_model.addItems(sorted(mw.col.models.allNames()))
         self.ui.kanji_deck.addItems(sorted(mw.col.decks.allNames(False)))
@@ -65,22 +66,20 @@ class SettingsDialog(QDialog):
         mw.addonManager.writeConfig(__name__, self._config)
         self.accept()
 
-    def update_items(self, cb, requried=False):
-        currentText = cb.currentText()
-        cb.clear()
-        fields = set(sorted(self._get_selectable_kanji_fields() + [currentText]))
-        if not requried:
-            fields.insert(0, '----')
-
-        cb.addItems(fields)
-        cb.setCurrentText(currentText)
 
     def on_kanji_cb_activated(self, index):
         for cb in self._kanji_field_cbs:
-            if cb == self.ui.cb_kanji:
-                self.update_items(cb, True)
-            else:
-                self.update_items(cb)
+            currentText = cb.currentText()
+            is_optional = cb.itemText(0) == '----'
+
+            fields = self._get_selectable_kanji_fields(currentText)
+
+            if is_optional:
+                fields = ['----'] + fields
+
+            cb.clear()
+            cb.addItems(fields)
+            cb.setCurrentText(currentText)
 
 
     def _load_config(self):
@@ -94,31 +93,37 @@ class SettingsDialog(QDialog):
 
         kanji_model = mw.col.models.get(self._config['kanjidic']['mid'])
         if not kanji_model is None:
+
+            for cb in self._kanji_field_cbs:
+                cb.addItems(mw.col.models.fieldNames(kanji_model))
+                cb.setCurrentIndex(0)
+
             self.ui.kanji_model.setCurrentText(kanji_model['name'])
 
             if not self._config['kanjidic']['kanji']:
-                self.ui.cb_kanji.addItems([self._config['kanjidic']['kanji']])
+                self.ui.cb_kanji.setCurrentText(self._config['kanjidic']['kanji'])
             if not self._config['kanjidic']['meaning']:
-                self.ui.cb_meaning.addItems([self._config['kanjidic']['meaning']])
+                self.ui.cb_meaning.setCurrentText(self._config['kanjidic']['meaning'])
             if not self._config['kanjidic']['onyomi']:
-                self.ui.cb_onyomi.addItems([self._config['kanjidic']['onyomi']])
+                self.ui.cb_onyomi.setCurrentText(self._config['kanjidic']['onyomi'])
             if not self._config['kanjidic']['kunyomi']:
-                self.ui.cb_kunyomi.addItems([self._config['kanjidic']['kunyomi']])
+                self.ui.cb_kunyomi.setCurrentText(self._config['kanjidic']['kunyomi'])
             if not self._config['kanjidic']['strokecount']:
-                self.ui.cb_strokecount.addItems([self._config['kanjidic']['strokecount']])
+                self.ui.cb_strokecount.setCurrentText(self._config['kanjidic']['strokecount'])
             if not self._config['kanjidic']['radical']:
-                self.ui.cb_radical.addItems([self._config['kanjidic']['radical']])
+                self.ui.cb_radical.setCurrentText(self._config['kanjidic']['radical'])
             if not self._config['kanjidic']['frequency']:
-                self.ui.cb_frequency.addItems([self._config['kanjidic']['frequency']])
+                self.ui.cb_frequency.setCurrentText(self._config['kanjidic']['frequency'])
+        
+        self.on_kanji_cb_activated(0)
 
-        for cb in self._kanji_field_cbs:
-            cb.activated.emit(0)
 
-    def _get_selectable_kanji_fields(self):
+    def _get_selectable_kanji_fields(self, additional_item):
         all_fields = mw.col.models.fieldNames(mw.col.models.byName(self.ui.kanji_model.currentText()))
+         
         selected_fields = [cb.currentText() for cb in self._kanji_field_cbs]
+        selected_fields.remove(additional_item)
         return [field for field in all_fields if not field in selected_fields]
-
 
 
 class ConfigLoadError(Exception):
